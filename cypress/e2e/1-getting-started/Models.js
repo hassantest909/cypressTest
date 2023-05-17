2e1ef07472d9bbdd6e9f09031b85975be7b6d5d5
Hassan <magdynamic.test@gmail.com>
2023-05-17 03:19:17 -0700

    first commit

--- cypress/e2e/1-getting-started/Models.js
+++ cypress/e2e/1-getting-started/Models.js
@@ -6,3 +6,30 @@ export class ModelsDemo {
         this.Age = age;
     }
 }
+
+export class RequestPayload {
+    constructor(security, account, channel, terminal, retrievalReferenceNumber, payload, additionalInformation, checkSum) {
+      this.security = security;
+      this.account = account;
+      this.channel = channel;
+      this.terminal = terminal;
+      this.retrievalReferenceNumber = retrievalReferenceNumber;
+      this.payload = payload;
+      this.additionalInformation = additionalInformation;
+      this.checkSum = checkSum;
+    }
+  }
+  
+  export class Payload {
+    constructor(cnic, mobileNo) {
+      this.cnic = cnic;
+      this.mobileNo = mobileNo;
+    }
+  }
+  
+  export class AdditionalInformation {
+    constructor(infoKey, infoValue) {
+      this.infoKey = infoKey;
+      this.infoValue = infoValue;
+    }
+  }
--- cypress/e2e/1-getting-started/demoTest.cy.js
+++ cypress/e2e/1-getting-started/demoTest.cy.js
@@ -1,5 +1,5 @@
 import { urls } from "../API Agorz/url";
-import { ModelsDemo } from "./Models";
+import { ModelsDemo,Payload,RequestPayload,AdditionalInformation } from "./Models";
 import { BaseDemo } from "./baseDemo";
 
 const basedemo = new BaseDemo()
@@ -7,7 +7,7 @@ const modeldemo = new ModelsDemo('hassan','100','24');
 
 describe('Only for demo',()=>{
 
-    it('https://dummy.restapiexample.com/api/v1/create',()=>{
+    it.only('https://dummy.restapiexample.com/api/v1/create',()=>{
 
         basedemo.callApi(modeldemo,urls.demoUrl).then((response)=>{
             expect(response.body).to.have.property('status','success')
@@ -15,5 +15,59 @@ describe('Only for demo',()=>{
             cy.url('contains','api')
         });
     });
+
+    it('console log',()=>{
+        const json = {
+            "security": {
+              "userName": "string",
+              "password": "string",
+              "securityToken": "string"
+            },
+            "account": {
+              "msidn": "string",
+              "iban": "string",
+              "bban": "string",
+              "pan": "string",
+              "currency": "string"
+            },
+            "channel": "string",
+            "terminal": "string",
+            "reterivalReferenceNumber": "string",
+            "payLoad": {
+              "cnic": "3240245976765",
+              "mobileNo": "03467199809"
+            },
+            "additionalInformation": [
+              {
+                "infoKey": "string",
+                "infoValue": "string"
+              }
+            ],
+            "checkSum": "string"
+          };
+          
+          const payload = new Payload(json.payLoad.cnic, json.payLoad.mobileNo);
+          const additionalInformation = json.additionalInformation.map(item => new AdditionalInformation(item.infoKey, item.infoValue));
+          const requestPayload = new RequestPayload(json.security, json.account, json.channel, json.terminal, json.reterivalReferenceNumber, payload, additionalInformation, json.checkSum);
+          
+          console.log(JSON.stringify(requestPayload));
+    })
+
+
+    it('fixtures',()=>{
+
+        cy.fixture('example2.json').then((fixture) => {
+           
+            fixture.security.userName = 'test';
+            // Use the data sets in your test
+            //console.log(JSON.stringify(fixture));
+            //cy.log(dataSet1.age.toString());
+          
+            console.log(JSON.stringify(fixture));
+            //cy.log(dataSet2.content);
+          });
+
+    })
+
 
 });
